3347f2534cdb19f1626f46acacdb724903c921de
Arya Majiah <71741042+aryatriputram@users.noreply.github.com>
2021-11-14 14:48:13 +0700

    final

---
+++ src/component/character.tsx
@@ -0,0 +1,22 @@
+import "@google/model-viewer";
+
+const Character = () => {
+    return (
+        <div>
+            {/* <model-viewer
+                bounds="tight"
+                src="char.glb"
+                ar ar-modes="webxr scene-viewer quick-look"
+                camera-controls environment-image="neutral"
+                poster="poster.webp"
+                shadow-intensity="2.2"
+                shadow-softness="1"
+                min-camera-orbit="10deg auto auto"
+                max-camera-orbit="98deg auto auto"
+                exposure="1">
+            </model-viewer> */}
+        </div>
+    );
+}
+
+export default Character;
